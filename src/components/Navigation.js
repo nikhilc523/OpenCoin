76beec9b8a92009122fd9cf12a8f004e77636350
Nikhil <nikhilchowdary523@icloud.com>
2023-10-22 23:37:25 +0530

    Initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav
+      className="w-[40%] mt-16 flex justify-around align-middle
+    border border-amber rounded-lg
+    "
+    >
+      <NavLink
+        to="/"
+        end
+        className={({ isActive }) => {
+          return `w-full text-base text-center font-nunito m-2.5
+
+${
+  isActive
+    ? "bg-amber text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-amber active:bg-amber active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+      >
+        Crypto
+      </NavLink>
+
+      <NavLink
+        to="/trending"
+        className={({ isActive }) => {
+          return `w-full text-base text-center font-nunito m-2.5
+
+${
+  isActive
+    ? "bg-amber text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-amber active:bg-amber active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+      >
+        trending
+      </NavLink>
+
+      <NavLink
+        to="/saved"
+        className={({ isActive }) => {
+          return `w-full text-base text-center font-nunito m-2.5
+
+${
+  isActive
+    ? "bg-amber text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-amber active:bg-amber active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+      >
+        saved
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navigation;
